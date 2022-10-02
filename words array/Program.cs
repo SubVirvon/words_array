e9c0912a249b6d2aaefbf28cc4f8888ab047bfa5
Virvon <dan130205@gmail.com>
2022-10-02 21:17:50 +0300

    words array

---
+++ words array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace words_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+
+            string[] words = Console.ReadLine().Split(' ');
+
+            foreach(var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
